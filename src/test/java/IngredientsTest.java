57a3eae5a26b1dac2157c70de64e836ef66c195b
Anna Gareeva <roromia@mail.ru>
2022-04-26 21:07:44 +0300

    Добавлен класс с настройками Протестирована работоспособность простого теста

--- src/test/java/IngredientsTest.java
+++ src/test/java/IngredientsTest.java
@@ -1,13 +1,20 @@
+import com.ya.BurgerRestClient;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class IngredientsTest {
+public class IngredientsTest extends BurgerRestClient {
 
+    private static final String COURIER_PATH = "ingredients";
 
-    ValidatableResponse createResponse = given()
-            .get()
-            .then();
-    
+    @Test
+    public void test(){
+        ValidatableResponse createResponse = given()
+                .spec(getBaseSpec())
+                .get(COURIER_PATH)
+                .then();
+        createResponse.log().body();
+    }
 
 }
